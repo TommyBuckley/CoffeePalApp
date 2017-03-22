ff6b4080036bb982688d047e4bc54d6be0f5a907
TommyBuckley <tommybuckley@icloud.com>
2017-03-22 02:27:45 +0000

    Updated

--- app/src/main/java/ie/coffeepal/coffeepal/activities/UserProfileActivity.java
+++ app/src/main/java/ie/coffeepal/coffeepal/activities/UserProfileActivity.java
@@ -1,7 +1,17 @@
 package ie.coffeepal.coffeepal.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 /**
  * Created by Tommy on 15/03/2017.
  */
