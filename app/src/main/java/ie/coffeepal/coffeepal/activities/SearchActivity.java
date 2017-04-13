82cc6535870ad58973e28eaca8d2508849a4e870
TommyBuckley <tommybuckley@icloud.com>
2017-04-13 13:13:41 +0100

    Updated

---
+++ app/src/main/java/ie/coffeepal/coffeepal/activities/SearchActivity.java
@@ -0,0 +1,62 @@
+package ie.coffeepal.coffeepal.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+import ie.coffeepal.coffeepal.R;
+
+/**
+ * Created by Tommy on 13/04/2017.
+ */
+
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private AppCompatButton searchButton;
+    private AppCompatButton nearMeButton;
+    private AppCompatButton topRatedButton;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchcafe);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners();
+    }
+    ////
+    private void initViews() {
+
+
+        searchButton = (AppCompatButton) findViewById(R.id.searchButton);
+        nearMeButton= (AppCompatButton) findViewById(R.id.nearMeButton);
+        topRatedButton= (AppCompatButton) findViewById(R.id.topRatedButton);
+
+
+
+    }
+    private void initListeners() {
+        searchButton.setOnClickListener(this);
+        nearMeButton.setOnClickListener(this);
+        topRatedButton.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.searchButton:
+                // Navigate to RegisterActivity
+                Intent intentSearch = new Intent(getApplicationContext(), CafeResultsActivity.class);
+                startActivity(intentSearch);
+                break;
+            case R.id.nearMeButton:
+                Intent intentNear = new Intent(getApplicationContext()CafeResultsActivity.class);
+                startActivity(intentNear);
+                break;
+            case R.id.topRatedButton:
+                Intent intentTop = new Intent(getApplicationContext()CafeResultsActivity.class);
+                startActivity(intentTop);
+        }
+    }
